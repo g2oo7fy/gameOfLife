78385f55be8efa099502901251bff2685f0fd9a9
bjoern kessler <thefutarist@gmail.com>
2018-10-19 19:16:25 +0200

    matrix random generator for bool matrices

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # gameOfLife
+
+The Game of Life represents a model used in biology.
+
+The matrixType already provides a print function for any type of matrices
+and a random generator, that fills a n-sized matrix with randomly chosen boolian values.
--- matrixType.cpp
+++ matrixType.cpp
@@ -1,29 +1,58 @@
 #include <iostream>	
-#include <vector>	
+#include <vector>
+#include <random>
+#include <stdlib.h>
+
 using namespace std;
 
-typedef std::vector<std::vector<int>> Matrix;
+typedef vector<vector<bool>> Matrix;
+
+bool coin(){
+	random_device r;
+	mt19937 random_engine{ r()};
+	bernoulli_distribution coin_distribution{0.5};
+
+	return coin_distribution(random_engine);
+}
+
 
 void printVector( Matrix m ){
-	std::vector<std::vector<int>>::iterator i;
-	std::vector<int>::iterator j;
+	vector<vector<bool>>::iterator i;
+	vector<bool>::iterator j;
 	for ( i = m.begin() ; i < m.end() ; i++ ){
 		for ( j = (*i).begin() ; j < (*i).end() ; j++ ){
-			cout << " " << *j;
+			char c;
+			if ( *j == true ){
+				c = '*';
+			} else {
+				c = ' ';
+			}
+			cout << " " << c;
 		}
-		cout << std::endl;
+		cout << endl;
 	}
 }
 
+Matrix randomMatrix( int n ){
+	vector<vector<bool>> m(n);
+	for ( int i = 0 ; i < n ; i++ ){
+		for ( int j = 0 ; j < n ; j++ ){
+			//bool b = rand() % 2;
+			bool b = coin();
+			m[i].push_back(b);
+		}
+	}
+		
+
+	//Matrix m = {{ true }, {false}};
+	return m;
+}
+
 int main(){
-//	Matrix m = {{ 1.0, 0.0, 0.3 }, 
-//		{ 0.2, 0.1, 1.0 },
-//		{ 0.0, 1.0, 0.7 }};
-	Matrix m = {{ 1, 0, 3 },
-		{ 2, 1, 1 },
-		{ 0, 1, 7 }};
+//	Matrix m = {{ 1, 0, 3 },
+//		{ 2, 1, 1 },
+//		{ 0, 1, 7 }};
 
-
-	printVector(m);
+	printVector( randomMatrix( 3 ));
 	return 0;
 }
